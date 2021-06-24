c500aedbf540554d98e5a842de3633e5e780bb48
Loakesh I <loakesh.indiran@wowlabz.com>
2021-06-24 17:49:59 +0530

    Fix: Constants Issue for the pallet functionalities Implemented:
    Because:

--- src/View/Modules/DashBoard/constants.js
+++ src/View/Modules/DashBoard/constants.js
@@ -1,10 +1,10 @@
 export const GET_TASKS = "GET_TASKS";
 
 export const FORM_TYPES = {
-    CREATE_TASK: { type: "createTask", title: "Create Task" },
-    BID_FOR_TASK: { type: "bidForTask", title: "Bid Task" },
-    COMPLETE_TASK: { type: "completeTask", title: "Complete Task" },
-    APPROVE_TASK: { type: "approveTask", title: "Approve Task" },
+    CREATE_TASK: { type: "Create New Task", title: "Create Task" },
+    BID_FOR_TASK: { type: "Bid", title: "Bid Task" },
+    COMPLETE_TASK: { type: "Complete", title: "Complete Task" },
+    APPROVE_TASK: { type: "Approve", title: "Approve Task" },
 };
 
 export const DEFAULT_ACCOUNT_IDS = {
